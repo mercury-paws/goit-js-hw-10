8623bf6934dcb932368887613c03cd323d3982f9
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-02-15 19:35:08 +0100

    js 10 hm 1

---
+++ src/js/1-timer.js
@@ -0,0 +1,23 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const fp = flatpickr('#datetime-picker', {});
+
+let userSelectedDate;
+
+iziToast.show({
+  title: 'Hey',
+  message: 'What would you like to add?',
+});
