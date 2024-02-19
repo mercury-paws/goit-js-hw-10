5d137172cfecaac5f4de680f967ca0dd90cb264b
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-02-19 22:50:05 +0100

    js 10 hm 5

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,53 +1,56 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const submitBtn = document.querySelector('button[type="submit"]');
-const radioFulfilled = document.querySelector('input[value="fulfilled"]');
-const radioRejected = document.querySelector('input[value="rejected"]');
-const inputNumber = document.querySelector('input[name="delay"]');
+const form = document.querySelector('.form');
 
-function fulfilledPromiceHandler(event) {
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
   event.preventDefault();
-}
 
-function rejectedPromiceHandler(event) {
-  event.preventDefault();
-}
+  const delayInput = document.querySelector('input[name="delay"]');
+  const delay = parseInt(delayInput.value);
 
-function submitBtnHandler(event) {
-  event.preventDefault();
-}
-
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve(
-        iziToast.error({
-          color: 'red',
-          message: 'Fulfilled promise in ${delay}ms',
-          position: 'topRight',
-          progressBarColor: 'rgb(0, 255, 184)',
-          timeout: `${delay}`,
-        })
-      );
-
-      //
-    } else {
-      reject(
-        iziToast.error({
-          color: 'red',
-          message: 'Rejected promise in ${delay}ms',
-          position: 'topRight',
-          progressBarColor: 'rgb(0, 255, 184)',
-          timeout: `${delay}`,
-        })
-      );
+  const stateInputs = document.querySelectorAll('input[name="state"]');
+  let state;
+  stateInputs.forEach(input => {
+    if (input.checked) {
+      state = input.value;
     }
-  }, ${delay});
-});
+  });
 
-// Registering promise callbacks
-promise
-  .then(value => console.log(value)) // "Success! Value passed to resolve function"
-  .catch(error => console.log(error)) // "Error! Error passed to reject function"
-  .finally(() => console.log('Promise settled')); // "Promise settled"
+  if (!delay || !state) {
+    console.log('Please fill out all fields');
+    return;
+  }
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(Error());
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.error({
+        color: 'green',
+        message: `Fulfilled promise in ${delay} ms`,
+        position: 'topRight',
+        progressBarColor: 'rgb(0, 255, 184)',
+        timeout: 1000,
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        color: 'red',
+        message: `Rejected promise in ${delay} ms`,
+        position: 'topRight',
+        progressBarColor: 'rgb(0, 255, 184)',
+        timeout: 1000,
+      });
+    });
+}
